98f21029659e4a663dfe84abb3b249f1c3135330
RickDvn <168721035+RickDvn@users.noreply.github.com>
2024-05-07 16:16:28 +0200

    Listener para el job

---
+++ src/main/java/com/viewnext/batchxlsx/listener/JobCompletionNotificationListener.java
@@ -0,0 +1,35 @@
+package com.viewnext.batchxlsx.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.jdbc.core.DataClassRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ * Clase listener que se usa para loggear lo cambiado despues de que el Job se cumpla
+ */
+@Component
+public class JobCompletionNotificationListener implements JobExecutionListener {
+
+	private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
+
+	private final JdbcTemplate jdbcTemplate;
+
+	public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	/**
+	 * Realiza unlog de los Stock de la base de datos cuando se completa exitosamente el Job
+	 */
+	@Override
+	public void afterJob(JobExecution jobExecution) {
+		if(jobExecution.getStatus() == BatchStatus.COMPLETED) {
+			log.info("!!! JOB FINISHED! Time to verify the results");
+		}
+	}
+}
